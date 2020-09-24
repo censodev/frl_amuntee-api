271df40e0ceb20836e4da0d4017b1a8b4eb58c8b
Phuong Bui <51000027+censodev@users.noreply.github.com>
2020-09-24 22:08:59 +0700

    init

---
+++ business/src/test/java/com/amuntee/business/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amuntee.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
