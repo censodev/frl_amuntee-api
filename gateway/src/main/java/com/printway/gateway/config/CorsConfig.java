e1815b430dffb959208fc7945c81d6e82ce528d5
Phuong Bui <51000027+censodev@users.noreply.github.com>
2020-10-13 00:02:47 +0700

    fix gateway

--- gateway/src/main/java/com/printway/gateway/config/CorsConfig.java
+++ gateway/src/main/java/com/printway/gateway/config/CorsConfig.java
@@ -12,7 +12,9 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
